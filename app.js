34bccbb52b225edd64b698c26a6649c181ad4b37
Chris-Bortel <bortelcc19@gmail.com>
2020-04-13 15:24:08 -0700

    Constructor fuction is created

--- app.js
+++ app.js
@@ -2,3 +2,56 @@
 
 console.log('i work');
 
+//*****TODO: Always******/
+// 1. find a target
+// 2. make the element/give it content
+// 3. append it to the target
+// 
+//As a user, I would like to display three unique products by chance so that the viewers can pick a favorite.
+
+// 1. Create a constructor function that creates an object associated with each product, and has the following properties:
+//    Name of the product
+//   TODO: File path of image
+var allProducts = []; // array for the products
+
+function Products(productName, imageSrc){
+  this.name = productName;
+  this.imageSrc = imageSrc;
+  this.clickCount = 0;
+
+  allProducts.push(this);// take the object that I am building and put it where I want it
+}
+
+// Products.prototype.render = function(){
+
+// }
+
+// 2. Create an algorithm that will randomly generate three unique product images from the images directory and display them side-by-side-by-side in the browser window.
+// function putNewProductsOnPage()
+
+// TODO: write a fucntion for 3 random numbers. (one first)
+//    could generate 1 rand numb and run three times
+//    generate 3 random numbers and put to an empty array
+//        return the array
+
+// Attach an event listener to the section of the HTML page where the images are going to be displayed.
+//  1. find a target
+//  2. make the element/give it content
+//  3. append it to the target
+
+// Once the users ‘clicks’ a product, generate three new products for the user to pick from.
+// As a user, I would like to track the selections made by viewers so that I can determine which products to keep for the catalog.
+// In the constructor function define a property to hold the number of times a product has been clicked.
+
+// After every selection by the viewer, update the newly added property to reflect if it was clicked.
+
+// As a user, I would like to control the number of rounds a user is presented with so that I can control the voting session duration.
+// By default, the user should be presented with 25 rounds of voting before ending the session.
+// Keep the number of rounds in a variable to allow the number to be easily changed for debugging and testing purposes.
+// As a user, I would like to view a report of results after all rounds of voting have concluded so that I can evaluate which products were the most popular.
+// Create a property attached to the constructor function itself that keeps track of all the products that are currently being considered.
+
+// After voting rounds have been completed, remove the event listeners on the product.
+
+// Display the list of all the products followed by the votes received and number of times seen for each. Example: Banana Slicer had 3 votes and was shown 5 times
+
